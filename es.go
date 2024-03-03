f95ecf7c1b504bb0993c3bc9f0b7d0291da76e29
cyj <13171230+chenyouji666@user.noreply.gitee.com>
2024-03-03 19:29:02 +0800

    es7

--- es.go
+++ es.go
@@ -1,4 +1,4 @@
-package es7
+package elasticsearch7
 
 import (
 	"context"
@@ -9,66 +9,109 @@ import (
 var (
 	esClient *elastic.Client
 	ctx      = context.Background()
-	err      error
+	esUrl    = "http://127.0.0.1:9200"
 )
 
+// 实例化es客户端
 func EsInit() {
-	//实例化es客户端
-	//SetSniff 检查集群中是否有其它可用节点
-	esClient, err = elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"), elastic.SetSniff(false))
+	var err error
+	esClient, err = elastic.NewClient(elastic.SetURL(esUrl), elastic.SetSniff(false))
 	if err != nil {
-		//  no Elasticsearch node available
-		// es没开或者没有指定连接地址
 		panic(err)
 	}
 }
 
-// 索引是否存在
-// indexName 索引的名字
+// 判断索引是否存在
 func ExistIndex(indexName string) (bool, error) {
 	exists, err := esClient.IndexExists(indexName).Do(ctx)
 	return exists, err
 }
 
 // 创建索引
-// indexName 索引的名字
+// indexName 索引名称,mapping 映射的结构体
 func CreateIndex(indexName string, mapping string) (*elastic.IndicesCreateResult, error) {
-
 	createIndex, err := esClient.CreateIndex(indexName).BodyString(mapping).Do(ctx)
-
 	return createIndex, err
 }
 
 // 向索引写入单条数据
-func AddDocToIndex(indexName string, id int, doc interface{}) (*elastic.IndexResponse, error) {
-	put1, err := esClient.Index().
+func AddDocToIndex(indexName string, id int, doc interface{}) error {
+	_, err := esClient.Index().
 		Index(indexName).
 		Id(strconv.Itoa(id)).
 		BodyJson(doc).
 		Do(ctx)
-
-	return put1, err
+	return err
 }
 
 // 根据文档id查询数据
 func SearchDocByDocID(indexName string, id int) (*elastic.GetResult, error) {
-	// Get tweet with specified ID
-	get1, err := esClient.Get().
+	result, err := esClient.Get().
 		Index(indexName).
 		Id(strconv.Itoa(id)).
 		Do(ctx)
-	return get1, err
+	return result, err
 }
 
 // 精确查询,term是精确查询，字段类型keyword 不能是text
 func TermQuery(indexName, field, value string, offset, limit int) (*elastic.SearchResult, error) {
 	termQuery := elastic.NewTermQuery(field, value)
-	searchResult, err := esClient.Search().
-		Index(indexName).         // search in index "twitter"
-		Query(termQuery).         // specify the query
-		From(offset).Size(limit). // take documents 0-9
-		Pretty(true).             // pretty print request and response JSON
-		Do(ctx)                   // execute
+	result, err := esClient.Search().
+		Index(indexName).
+		Query(termQuery).
+		From(offset).Size(limit).
+		Pretty(true).
+		Do(ctx)
+	return result, err
+}
 
-	return searchResult, err
+// 通过文档ID更改信息
+func UpdateByDocId(indexName string, id int, doc interface{}) error {
+	_, err := esClient.Update().
+		Index(indexName).
+		Id(strconv.Itoa(id)).
+		Doc(doc).
+		Do(ctx)
+
+	return err
+}
+
+// 词项多条件精确查询
+func TermsQuery(indexName, field string, offset, limit int, values ...interface{}) (*elastic.SearchResult, error) {
+	termQuery := elastic.NewTermsQuery(field, values...)
+	result, err := esClient.Search().
+		Index(indexName).
+		Query(termQuery).
+		From(offset).Size(limit).
+		Pretty(true).
+		Do(ctx)
+	return result, err
+}
+
+// 词项的区间查询
+func RangeQuery(indexName, field string, offset, limit int, gte, lte interface{}) (*elastic.SearchResult, error) {
+	rangeQuery := elastic.NewRangeQuery(field).Gte(gte).Lte(lte)
+	result, err := esClient.Search().
+		Index(indexName).
+		Query(rangeQuery).
+		From(offset).Size(limit).
+		Pretty(true).
+		Do(ctx)
+	return result, err
+}
+
+// 高亮搜索
+func searchWithHighlight(indexName, field, msg string, offset, limit int) (*elastic.SearchResult, error) {
+	query := elastic.NewMatchQuery(field, msg)
+	highlight := elastic.NewHighlight().Field("message")
+	highlight.PostTags("<span color='red'>")
+	highlight.PreTags("</span>")
+	result, err := esClient.Search().
+		Index(indexName).
+		Query(query).
+		Highlight(highlight).
+		From(offset).Size(limit).
+		Pretty(true).
+		Do(ctx)
+	return result, err
 }
